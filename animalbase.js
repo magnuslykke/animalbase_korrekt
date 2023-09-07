61ee038c8b70db44c9887a50902aa1dee9eb6e93
magnuslykke <magn965m@stud.kea.dk>
2023-09-07 11:49:27 +0200

    så er den næsten færdig

--- animalbase.js
+++ animalbase.js
@@ -94,7 +94,12 @@ function isDog(animal) {
 function selectSort(event) {
   const sortBy = event.target.dataset.sort;
   const sortDir = event.target.dataset.sortDirection;
+  // find old "sort by" element, and remove sortBy.
+  const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+  oldElement.classList.remove("sortby");
 
+  // Indicate active sort:
+  event.target.classList.add("sortby");
   //toggle the direction
   if (sortDir === "asc") {
     event.target.dataset.sortDirection = "desc";
